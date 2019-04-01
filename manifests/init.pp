f9e85b68095a9e6d66e38d24f37ea5b30c742bac
mahendra-ring <47548350+mahendra-ring@users.noreply.github.com>
2019-04-01 17:26:59 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,22 @@
 # @example
 #   include webnode
 class webnode {
+file { '/appfs/webnode':
+    ensure => 'directory',
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0755',
+  }
+file { '/appfs/webnode':
+    ensure  => present,
+    mode    => '0755',
+    source => "puppet:///modules/webnode/${::hostname.js}",
+  }
+  exec { 'noderun':
+    command => 'node /appfs/webnode/${::hostname.js}',
+    path    => '/usr/local/bin/:/bin/',
+    # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
+  }
+
+
 }
